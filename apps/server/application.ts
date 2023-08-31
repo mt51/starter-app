7f7af162e8079f3daadc128cd271144a3f8f4083
ike.liu <ike.liu@zoom.us>
2023-08-31 12:59:12 +0800

    complete developemnt setting

--- apps/server/application.ts
+++ apps/server/application.ts
@@ -1,15 +1,24 @@
+import fs from 'fs';
+import path from 'path';
 import http from 'http'
 import Koa from 'koa'
 import koaBodyParser from 'koa-bodyparser'
+import koaConnect from 'koa-connect'
+import koaSend from 'koa-send';
 import cors from '@koa/cors'
 import { useContainer, useKoaServer } from 'routing-controllers'
 import { Container, Service } from 'typedi'
+import { createServer } from 'vite';
 import { ApplicationController } from './controller';
 
 @Service()
 export default class App {
   private app: Koa | null = null;
   private server: http.Server | null = null
+
+  constructor() {
+    this.app = this.initKoa();
+  }
 
   private initKoa() {
     useContainer(Container)
@@ -24,6 +33,7 @@ export default class App {
     app.use(cors())
 
     useKoaServer(app, {
+      routePrefix: '/api',
       controllers: [
         ApplicationController,
       ]
@@ -33,11 +43,67 @@ export default class App {
     return app
   }
 
-  public start() {
-    const app = this.initKoa()
-    this.server = http.createServer(app.callback())
+  public async start() {
+
+    if (process.env.NODE_ENV === 'development') {
+      await this.startDev();
+    } else {
+      await this.startProd();
+    }
+
+    this.server = http.createServer(this.app!.callback())
+
     this.server?.listen(5178, () => {
       console.log('app is running at 5178')
+    })
+  }
+
+  public async startProd() {
+    const template = fs.readFileSync('dist/index.html', 'utf-8');
+    this.app?.use(async(ctx, next) => {
+      if (ctx.path.startsWith('/assets')) {
+        await koaSend(ctx, ctx.path, { root: path.resolve(__dirname)})
+        return
+      }
+
+      if (ctx.path.startsWith('/api')) {
+        next();
+        return;
+      }
+
+       ctx.type = 'text/html';
+       ctx.body = template;
+       return
+    })
+  }
+
+  public async startDev() {
+    const viteServer = await createServer({
+      root: path.resolve(__dirname, '../client'),
+      logLevel: "error",
+      server: {
+        middlewareMode: true
+      },
+      appType: 'custom'
+    })
+
+    this.app?.use(koaConnect(viteServer.middlewares))
+
+    this.app?.use(async (ctx, next) => {
+      if (ctx.request.url.startsWith('/api')) {
+        next();
+      } else {
+        try {
+          let template = fs.readFileSync(path.resolve(__dirname, '../client/index.html'), 'utf-8')
+          template = await viteServer.transformIndexHtml(ctx.path, template)
+          ctx.type = 'text/html'
+          ctx.body = template
+        } catch(e: any) {
+          viteServer && viteServer.ssrFixStacktrace(e);
+          console.log(e.stack)
+          ctx.throw(500, e.stack)
+        }
+      }
     })
   }
 }
--- apps/server/start.ts
+++ apps/server/start.ts
@@ -1,7 +1,14 @@
 import 'reflect-metadata';
 import { Container } from 'typedi';
-import Application from './Application';
+import Application from './application';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 const app = Container.get(Application)
 
-app.start();
+if (isDevelopment) {
+  app.startDev();
+} else {
+  app.start();
+}
+
