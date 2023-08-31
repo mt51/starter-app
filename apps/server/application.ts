022d1b29ecda8efa76a7b00a67922ba004f5ab20
ike.liu <ike.liu@zoom.us>
2023-08-31 17:39:18 +0800

    update development

--- apps/server/application.ts
+++ apps/server/application.ts
@@ -1,69 +1,66 @@
 import fs from 'fs';
 import path from 'path';
-import http from 'http'
-import Koa from 'koa'
-import koaBodyParser from 'koa-bodyparser'
-import koaConnect from 'koa-connect'
+import http from 'http';
+import Koa from 'koa';
+import koaBodyParser from 'koa-bodyparser';
+import koaConnect from 'koa-connect';
 import koaSend from 'koa-send';
-import cors from '@koa/cors'
-import { useContainer, useKoaServer } from 'routing-controllers'
-import { Container, Service } from 'typedi'
+import cors from '@koa/cors';
+import { useContainer, useKoaServer } from 'routing-controllers';
+import { Container, Service } from 'typedi';
 import { createServer } from 'vite';
 import { ApplicationController } from './controller';
 
 @Service()
 export default class App {
   private app: Koa | null = null;
-  private server: http.Server | null = null
+  private server: http.Server | null = null;
 
   constructor() {
     this.app = this.initKoa();
   }
 
   private initKoa() {
-    useContainer(Container)
-    const app = new Koa()
+    useContainer(Container);
+    const app = new Koa();
     app.use(
       koaBodyParser({
         jsonLimit: '5mb',
-        formLimit: '5mb'
-      })
-    )
+        formLimit: '5mb',
+      }),
+    );
 
-    app.use(cors())
+    app.use(cors());
 
     useKoaServer(app, {
       routePrefix: '/api',
-      controllers: [
-        ApplicationController,
-      ]
-    })
+      controllers: [ApplicationController],
+    });
 
-    this.app = app
-    return app
+    this.app = app;
+    return app;
   }
 
   public async start() {
-
     if (process.env.NODE_ENV === 'development') {
       await this.startDev();
     } else {
       await this.startProd();
     }
 
-    this.server = http.createServer(this.app!.callback())
+    this.server = http.createServer(this.app!.callback());
 
     this.server?.listen(5178, () => {
-      console.log('app is running at 5178')
-    })
+      console.log('app is running at 5178');
+    });
   }
 
   public async startProd() {
     const template = fs.readFileSync('dist/index.html', 'utf-8');
-    this.app?.use(async(ctx, next) => {
+    this.app?.use(async (ctx, next) => {
       if (ctx.path.startsWith('/assets')) {
-        await koaSend(ctx, ctx.path, { root: path.resolve(__dirname)})
-        return
+        await koaSend(ctx, ctx.path, { root: path.resolve(__dirname) });
+        return;
       }
 
       if (ctx.path.startsWith('/api')) {
@@ -71,39 +68,42 @@ export default class App {
         return;
       }
 
-       ctx.type = 'text/html';
-       ctx.body = template;
-       return
-    })
+      ctx.type = 'text/html';
+      ctx.body = template;
+      return;
+    });
   }
 
   public async startDev() {
     const viteServer = await createServer({
       root: path.resolve(__dirname, '../client'),
-      logLevel: "error",
+      logLevel: 'error',
       server: {
-        middlewareMode: true
+        middlewareMode: true,
       },
-      appType: 'custom'
-    })
+      appType: 'custom',
+    });
 
-    this.app?.use(koaConnect(viteServer.middlewares))
+    this.app?.use(koaConnect(viteServer.middlewares));
 
     this.app?.use(async (ctx, next) => {
       if (ctx.request.url.startsWith('/api')) {
         next();
       } else {
         try {
-          let template = fs.readFileSync(path.resolve(__dirname, '../client/index.html'), 'utf-8')
-          template = await viteServer.transformIndexHtml(ctx.path, template)
-          ctx.type = 'text/html'
-          ctx.body = template
-        } catch(e: any) {
+          let template = fs.readFileSync(
+            path.resolve(__dirname, '../client/index.html'),
+            'utf-8',
+          );
+          template = await viteServer.transformIndexHtml(ctx.path, template);
+          ctx.type = 'text/html';
+          ctx.body = template;
+        } catch (e: any) {
           viteServer && viteServer.ssrFixStacktrace(e);
-          console.log(e.stack)
-          ctx.throw(500, e.stack)
+          console.log(e.stack);
+          ctx.throw(500, e.stack);
         }
       }
-    })
+    });
   }
 }
--- apps/server/start.ts
+++ apps/server/start.ts
@@ -2,13 +2,6 @@ import 'reflect-metadata';
 import { Container } from 'typedi';
 import Application from './application';
 
-const isDevelopment = process.env.NODE_ENV === 'development';
+const app = Container.get(Application);
 
-const app = Container.get(Application)
-
-if (isDevelopment) {
-  app.startDev();
-} else {
-  app.start();
-}
-
+app.start();
